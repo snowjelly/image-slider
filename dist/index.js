16c4a15d270eb71b61581e1fe9ed4b36178a3834
Connor Broadwell <broadwellconnor@gmail.com>
2023-05-02 15:25:09 -0500

    nextImage and PreviousImage functions

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,25 @@ carouselImages.forEach((img) => {
     img.setAttribute('active', '');
   }
 });
+
+const nextImage = () => {
+  for (let i=0;i<carouselImages.length;i++) {
+    if (i === carouselImages.length - 1) return;
+    if (carouselImages[i].getAttribute('active') !== null) {
+      carouselImages[i + 1].setAttribute('active', '');
+      carouselImages[i].removeAttribute('active');
+      return;
+    }
+  }
+}
+
+const previousImage = () => {
+  for (let i=carouselImages.length - 1;i>=0;i--) {
+    if (carouselImages[i].getAttribute('active') !== null) {
+      if (i === 0) return;
+      carouselImages[i - 1].setAttribute('active', '');
+      carouselImages[i].removeAttribute('active');
+      return;
+    }
+  }
+}
